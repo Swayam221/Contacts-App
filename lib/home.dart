75b1285b66016a16828b67c621851a0d710c3732
Swayam221 <68892315+Swayam221@users.noreply.github.com>
2021-10-24 14:18:02 +0530

    added basic functionality - view contacts screen

---
+++ lib/home.dart
@@ -0,0 +1,95 @@
+import 'package:contacts_app/services/api_calls.dart';
+import 'package:contacts_app/widgets/contact_card.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'models/Contact.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  List<Contact> contacts = [];
+  var gridController = ScrollController();
+  var searchController = TextEditingController();
+  @override
+  void initSate(){
+    super.initState();
+    gridController.addListener((){
+      if(gridController.position.pixels == gridController.position.maxScrollExtent)
+      {
+
+      }
+    });
+  }
+
+  Future<void> getContacts() async
+  {
+    contacts = await Services.getContacts();
+    setState(() {
+      
+    });
+  }
+
+  @override
+  void didChangeDependencies()
+  {
+    super.didChangeDependencies();
+  }
+
+  @override 
+  Widget build(BuildContext context)
+  {
+    getContacts();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5,
+        title: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.white.withOpacity(0.8),
+          ),
+          child: TextField(
+            controller: searchController,
+            decoration: InputDecoration(
+              hintText: "Search",
+              border: InputBorder.none,
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: (val) async
+            {},
+          ),
+        ),
+      ),
+      body: Column(
+          children: [
+            Flexible(
+              fit: FlexFit.loose,
+              child:GridView.builder(
+              controller: gridController,
+              primary: false,physics: BouncingScrollPhysics(),
+              padding: const EdgeInsets.all(20),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: !kIsWeb?2:4,
+                // children: books.map((i) => BookCard(title: i.title,author: i.author,publishDate: i.datePublished)).toList(),
+              ),
+              itemCount: contacts.length,
+              itemBuilder: (context,index) {
+                return ContactCard(firstName: contacts[index].firstName,lastName: contacts[index].lastName,email: contacts[index].email,);
+              },
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
--- lib/widgets/contact_card.dart
+++ lib/widgets/contact_card.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class BookCard extends StatelessWidget{
+class ContactCard extends StatelessWidget{
   final firstName;
   final lastName;
   final email;
-  BookCard({this.firstName,this.lastName,this.email});
+  ContactCard({this.firstName,this.lastName,this.email});
   
   @override
   Widget build(BuildContext context) {
@@ -14,12 +14,25 @@ class BookCard extends StatelessWidget{
         //mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(height: 15,),
           CircleAvatar(
+            radius: 37,
             backgroundColor: Colors.purple,
-            child: Text(firstName[0]+lastName!=""?lastName[0]:""),
+            child: Text(firstName[0]+(lastName!=""?lastName[0]:""), style: TextStyle(fontSize: 35),),
           ),
-          Text(firstName+"  "+lastName!=""?lastName:"")
+          SizedBox(height: 10,),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(firstName+" "+(lastName!=""?lastName:""),
+                style: TextStyle(fontSize: 20),),
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
         ],
       ),
     );
